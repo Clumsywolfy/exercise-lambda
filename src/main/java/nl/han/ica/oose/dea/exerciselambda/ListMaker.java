293dda12eaef604ed0e3022bc9d2a7b810c923a2
Clumsywolfy <debbie.kauw@hotmail.nl>
2021-02-11 08:29:32 +0100

    Update ListMaker.java
    Gemaakt stap 2

--- src/main/java/nl/han/ica/oose/dea/exerciselambda/ListMaker.java
+++ src/main/java/nl/han/ica/oose/dea/exerciselambda/ListMaker.java
@@ -10,61 +10,56 @@ import java.util.List;
 
 public class ListMaker {
 
-    /**
-     * Create a {@link List} containing only the Persons that are both male and adult.
-     *
-     * @param allPersons A {@link List} of {@link Person} Objects
-     * @return A {@link List} containing only the Persons that are both male and adult
-     */
+    int adult = 18;
+
     public List<Person> createMaleAdultList(List<Person> allPersons) {
 
         if (allPersons == null) {
             return new ArrayList<>();
         }
-
         List<Person> filteredMaleAdults = new ArrayList<>();
 
         for (Person person : allPersons) {
-            if (person.getGender().equals(Gender.MALE)) {
-                LocalDate now = LocalDate.now();
-                Period age = Period.between(person.getBirthDate(), now);
-
-                if (age.getYears() > 18) {
+            if (checkGender(person) == Gender.MALE) {
+                if (checkAge(person)) {
                     filteredMaleAdults.add(person);
                 }
-
             }
         }
         return filteredMaleAdults;
     }
 
-    /**
-     * Create a {@link List} containing only the Persons that are both female and adult.
-     *
-     * @param allPersons A {@link List} of {@link Person} Objects
-     * @return A {@link List} containing only the Persons that are both female and adult
-     */
     public List<Person> createFemaleAdultList(List<Person> allPersons) {
-
-        int adult = 18;
 
         if (allPersons == null) {
             return new ArrayList<>();
         }
-
         List<Person> filteredFemaleAdults = new ArrayList<>();
 
         for (Person person : allPersons) {
-            if (person.getGender().equals(Gender.FEMALE)) {
-                LocalDate now = LocalDate.now();
-                Period age = Period.between(person.getBirthDate(), now);
-
-                if (age.getYears() > adult) {
+            if (checkGender(person) == Gender.FEMALE) {
+                if (checkAge(person)) {
                     filteredFemaleAdults.add(person);
                 }
             }
         }
         return filteredFemaleAdults;
     }
+    public boolean checkAge(Person person){
+        LocalDate now = LocalDate.now();
+        Period age = Period.between(person.getBirthDate(), now);
+        if (age.getYears() > adult) {
+            return true;
+        }
+        return false;
+    }
 
+    public Gender checkGender(Person person){
+        if (person.getGender().equals(Gender.FEMALE)){
+            return Gender.FEMALE;
+        } else if (person.getGender().equals(Gender.MALE)){
+            return Gender.MALE;
+        }
+        return null;
+    }
 }
